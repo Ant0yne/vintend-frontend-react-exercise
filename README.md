55ec8bd84f45cffef32975309eef5f9847a1334a
Ant0yne <antoine.soliv@gmail.com>
2024-03-27 13:54:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Frontend Exercises - Vinted
+
+An exercise to recreate the Vinted website. It's part of my training to become a web and mobile developer with the school "Le Reacteur"
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<Router>
+				<Routes></Routes>
+			</Router>
+		</>
+	);
+}
+
+export default App;
