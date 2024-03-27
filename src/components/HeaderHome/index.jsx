556d96e96f9cbb873a4ecc8d23a4273aab4ed992
Ant0yne <antoine.soliv@gmail.com>
2024-03-27 21:49:38 +0100

    home-full-responsive

--- src/components/HeaderHome/index.jsx
+++ src/components/HeaderHome/index.jsx
@@ -4,30 +4,32 @@ const HeaderHome = () => {
 	return (
 		<>
 			<header id="header-home">
-				<div id="header-home-logo">
-					<img
-						src="https://lereacteur-vinted.netlify.app/static/media/logo.10b0caad793dd0a8ea72.png"
-						alt=""
-					/>
-					<nav className="menu-w">
-						<button>Menu</button>
+				<div className="container">
+					<div id="header-home-logo">
+						<img
+							src="https://lereacteur-vinted.netlify.app/static/media/logo.10b0caad793dd0a8ea72.png"
+							alt=""
+						/>
+						<nav className="menu-w">
+							<button>Menu</button>
+						</nav>
+					</div>
+					<div>
+						<input
+							type="text"
+							name="search-bar"
+							id="search-bar"
+							placeholder="Rechercher des articles"
+						/>
+					</div>
+					<nav className="header-m">
+						<div>
+							<button> S'inscrire</button>
+							<button>Se connecter</button>
+						</div>
+						<button>Vends tes articles</button>
 					</nav>
 				</div>
-				<div>
-					<input
-						type="text"
-						name="search-bar"
-						id="search-bar"
-						placeholder="Rechercher des articles"
-					/>
-				</div>
-				<nav className="header-m">
-					<div>
-						<button> S'inscrire</button>
-						<button>Se connecter</button>
-					</div>
-					<button>Vends tes articles</button>
-				</nav>
 			</header>
 		</>
 	);
