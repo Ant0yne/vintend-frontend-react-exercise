92911cb7b4d1a6dfca9ef4252f806f0243278be9
Ant0yne <antoine.soliv@gmail.com>
2024-03-28 11:44:44 +0100

    torn-effect-hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -4,7 +4,9 @@ const Hero = () => {
 	return (
 		<>
 			<section id="hero">
-				<div id="hero-bg"></div>
+				<div id="hero-bg">
+					<div id="torn-effect"></div>
+				</div>
 				<div id="tri">
 					<h2>Prêts à faire du tri dans vos placards ?</h2>
 					<button>Commencer à vendre</button>
