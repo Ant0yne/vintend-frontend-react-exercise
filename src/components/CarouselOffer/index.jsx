4d59481b02ab9fa26e12a1e4a6f80f62a182b43e
Ant0yne <antoine.soliv@gmail.com>
2024-03-30 15:58:09 +0100

    carousel-offer-raw

--- src/components/CarouselOffer/index.jsx
+++ src/components/CarouselOffer/index.jsx
@@ -4,19 +4,21 @@ const CarouselOffer = ({ image, pictures }) => {
 	return (
 		<>
 			<nav className="carousel-offer">
-				<img id="image" src={image.secure_url} alt={image.asset_id} />
+				<div>
+					<img id="image" src={image.secure_url} alt={image.asset_id} />
 
-				{/* For the carousel later */}
+					{/* For the carousel later */}
 
-				{/* {pictures.map((picture) => {
-					return (
-						<img
-							key={picture.secure_url}
-							src={picture.secure_url}
-							alt={picture.asset_id}
-						/>
-					);
-				})} */}
+					{pictures.map((picture) => {
+						return (
+							<img
+								key={picture.secure_url}
+								src={picture.secure_url}
+								alt={picture.asset_id}
+							/>
+						);
+					})}
+				</div>
 			</nav>
 		</>
 	);
