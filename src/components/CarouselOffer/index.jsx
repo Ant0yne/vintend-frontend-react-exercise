8836179d3e19421704a5dcc5cda09c97c61dcc13
Ant0yne <antoine.soliv@gmail.com>
2024-04-01 12:20:47 +0200

    carousel-temp-layout

--- src/components/CarouselOffer/index.jsx
+++ src/components/CarouselOffer/index.jsx
@@ -1,24 +1,61 @@
+import Carousel from "react-multi-carousel";
+
+import "react-multi-carousel/lib/styles.css";
 import "./carouselOffer.css";
 
 const CarouselOffer = ({ image, pictures }) => {
+	const responsive = {
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 1,
+			slidesToSlide: 1,
+			partialVisibilityGutter: 40,
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 1,
+			slidesToSlide: 1,
+			partialVisibilityGutter: 40,
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1,
+			slidesToSlide: 1,
+			partialVisibilityGutter: 40,
+		},
+	};
+
 	return (
 		<>
 			<nav className="carousel-offer">
-				<div>
-					<img id="image" src={image.secure_url} alt={image.asset_id} />
-
-					{/* For the carousel later */}
-
+				<Carousel
+					swipeable={true}
+					draggable={false}
+					showDots={true}
+					responsive={responsive}
+					ssr={true}
+					infinite={true}
+					keyBoardControl={true}
+					customTransition="all .5"
+					transitionDuration={500}
+					containerClass="carousel-container"
+					removeArrowOnDeviceType={["tablet", "mobile"]}
+					deviceType={responsive}
+					dotListClass="custom-dot-list-style"
+					itemClass="carousel-item-padding-40-px"
+					// partialVisbile={true}
+					centerMode={true}>
+					<div>
+						<img id="image" src={image.secure_url} alt={image.asset_id} />
+					</div>
 					{pictures.map((picture) => {
 						return (
-							<img
-								key={picture.secure_url}
-								src={picture.secure_url}
-								alt={picture.asset_id}
-							/>
+							<div key={picture.secure_url}>
+								<img src={picture.secure_url} alt={picture.asset_id} />
+							</div>
 						);
 					})}
-				</div>
+				</Carousel>
 			</nav>
 		</>
 	);
