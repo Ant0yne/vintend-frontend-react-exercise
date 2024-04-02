255cb502acada5bec50e311d632f00a6ab601b55
Ant0yne <antoine.soliv@gmail.com>
2024-04-02 16:19:41 +0200

    payment-working

---
+++ src/components/CheckoutForm/index.jsx
@@ -0,0 +1,106 @@
+import {
+	PaymentElement,
+	useElements,
+	useStripe,
+} from "@stripe/react-stripe-js";
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./checkoutForm.css";
+
+const CheckoutForm = ({ id, title, price }) => {
+	// if error message when submit the payment form
+	const [errorMessage, setErrorMessage] = useState(null);
+	// waiting for response after submit payment
+	const [isLoading, setIsLoading] = useState(false);
+	// check if payment is done
+	const [paymentDone, setPaymentDone] = useState(false);
+
+	// To make the payment request to Stripe
+	const stripe = useStripe();
+	// To send the users input
+	const elements = useElements();
+
+	const handlePayment = async (e) => {
+		e.preventDefault();
+		// Start the payment process
+		setIsLoading(true);
+
+		try {
+			// if elements still a promise return
+			if (elements == null) {
+				return;
+			}
+
+			// Request to Stripe with all the inputs from the form to check
+			// If error return
+			const { error: submitError } = await elements.submit();
+			if (submitError) {
+				setErrorMessage(submitError.message);
+				setIsLoading(false);
+				return;
+			}
+
+			// Request to back the PaymentIntent creation
+			// If ok, return the clientSecret key
+			const response = await axios.post(
+				"https://lereacteur-vinted-api.herokuapp.com/v2/payment",
+				{
+					title: title,
+					amount: price,
+				}
+			);
+			const clientSecret = response.data.client_secret;
+
+			// Request for payment
+			const { error, paymentIntent } = await stripe.confirmPayment({
+				// Send the input from form and the PaymentIntent key
+				elements,
+				clientSecret,
+				// A route to redirect (blocked)
+				confirmParams: {
+					return_url: "https://vint-react-front-antoine-soliv.netlify.app/",
+				},
+				redirect: "if_required",
+			});
+
+			// Display the error if the payment failed
+			if (error) {
+				setErrorMessage(error.message);
+			}
+
+			if (paymentIntent.status === "succeeded") {
+				setPaymentDone(true);
+			}
+
+			setIsLoading(false);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return paymentDone ? (
+		<>
+			<p>Achat Terminé !</p>
+			<button>
+				<Link to="/">Retour à l'Acceuil</Link>
+			</button>
+		</>
+	) : (
+		<form onSubmit={handlePayment}>
+			<PaymentElement />
+			<button
+				id="submit-payment"
+				// Disable the submit if a payment try is in progress
+				// or if the PaymentElement is not loaded
+				disabled={!stripe || !elements || isLoading}>
+				Payer
+			</button>
+
+			{errorMessage && <p>{errorMessage}</p>}
+		</form>
+	);
+};
+
+export default CheckoutForm;
--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -4,6 +4,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 import HeaderHome from "../components/HeaderHome";
+import CheckoutForm from "../components/CheckoutForm";
 
 const stripePromise = loadStripe(
 	"pk_test_51HCObyDVswqktOkX6VVcoA7V2sjOJCUB4FBt3EOiAdSz5vWudpWxwcSY8z2feWXBq6lwMgAb5IVZZ1p84ntLq03H00LDVc2RwP"
@@ -17,9 +18,10 @@ const Payment = ({
 	preventRoute,
 	setPreventRoute,
 }) => {
-	// To not display the query components (switch and range)
 	const navigate = useNavigate();
+	// To not display the query components (switch and range)
 	const noQueryRoute = true;
+	// The props sent with the navigate to Payment
 	const location = useLocation();
 
 	useEffect(() => {
@@ -37,11 +39,12 @@ const Payment = ({
 
 	const { id, title, price } = location.state;
 
-	// const options = {
-	// 	mode: "payment",
-	// 	amount: Number((price * 100).toFixed(0)),
-	// 	currency: "eur",
-	// };
+	// Transaction's option
+	const options = {
+		mode: "payment",
+		amount: Number((price * 100).toFixed(0)),
+		currency: "eur",
+	};
 
 	return (
 		<>
@@ -54,6 +57,11 @@ const Payment = ({
 				preventRoute={preventRoute}
 				setPreventRoute={setPreventRoute}
 			/>
+			<main>
+				<Elements stripe={stripePromise} options={options}>
+					<CheckoutForm id={id} title={title} price={price} />
+				</Elements>
+			</main>
 		</>
 	);
 };
